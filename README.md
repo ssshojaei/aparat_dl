3c2e5329d9cccd1e6d04f4d0293ed926e57e6681
Saleh Shojaei <ordinarysaleh@gmail.com>
2020-07-15 03:19:38 +0430

    Rewritten with cheerio

---
+++ README.md
@@ -0,0 +1,30 @@
+# aparat_dl
+
+Download single video or playlists from Aparat
+
+![aparat_dl gif](https://raw.githubusercontent.com/ssshojaei/aparat_dl/master/screenshots/demo.gif)
+
+## Update
+
+- Updated for new aparat design
+- Faster and lighter than before
+- Ability to download single video
+- Fixed bugs
+
+## Get Started
+
+**Install**
+
+```
+sudo apt install node npm
+npm install --global aparat-dl
+```
+
+**Use**
+
+```
+aparat_dl https://www.aparat.com/v/XXXXX .
+```
+
+- param 1: 'aparat link', --required
+- param 2: 'download directory' --optional
---
+++ index.js
@@ -0,0 +1,58 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+const prompts = require('prompts')
+const {
+	isPlayList,
+	getPlayList,
+	getSingleVideo,
+	downloadVideo,
+	getInfo,
+} = require('./lib/aparat')
+
+const page = process.argv[2]
+const path = process.argv[3] || './'
+
+if (!page) {
+	console.error('error: Page url is required!')
+	return 0
+}
+console.info('--- Getting page data ---')
+axios.get(page).then(
+	(response) => {
+		if (response.status === 200) {
+			console.info('--- Initializing ---')
+			const html = response.data
+			const $ = cheerio.load(html)
+
+			if (isPlayList($)) {
+				//playlist
+				const videosList = getPlayList($)
+				videosList.map((item, index) => {
+					getInfo(item).then((res) => {
+						const highQuality = res.links.length - 1
+						downloadVideo(
+							res.links[highQuality].value,
+							`${index + 1} - ${res.title}`,
+							path
+						)
+					})
+				})
+			} else {
+				// single video
+				const video = getSingleVideo($)
+				;(async () => {
+					const quality = await prompts([
+						{
+							type: 'select',
+							name: 'link',
+							message: 'Pick a quality:',
+							choices: video.links,
+						},
+					])
+					downloadVideo(quality.link, video.title, path)
+				})()
+			}
+		}
+	},
+	(err) => console.log(err)
+)
